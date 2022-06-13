2b386e48a258430f392df05d5c25213f009aeb79
Kasia <kasiakos81@gmail.com>
2022-06-13 19:04:27 +0200

    commit z testami z 13 czerwca

--- src/test/java/rental/WypozyczalniaTest.java
+++ src/test/java/rental/WypozyczalniaTest.java
@@ -310,7 +310,7 @@ public class WypozyczalniaTest {
 
         List<Samochod> wynajete1 = wypozyczalnia.zwrocListeWynajetych();
         List<Samochod> wszystkie1 = wypozyczalnia.zwrocListe(); // to jest zwrocListe (wszystkich)
-        List<WynajemSamochodu> wynajmy1 = wypozyczalnia.listaAktywnychWynajmów(); // to jest zwrocListe (wszystkich)
+        List<WynajemSamochodu> wynajmy1 = wypozyczalnia.listaAktywnychWynajmów();
         Assert.assertEquals("Lista wynajętych powinna być pusta", 0, wynajete1.size());
         Assert.assertEquals("Lista wszystkich powinna być pusta", 0, wszystkie1.size());
         Assert.assertEquals("Lista aktywnych wynajmow powinna być pusta", 0, wynajmy1.size());
@@ -349,11 +349,83 @@ public class WypozyczalniaTest {
         Assert.assertEquals("Lista aktywnych wynajmow powinna byc pusta", 0, wynajmy3.size());
         Assert.assertEquals("Lista zakonczonych wynajmow powinna miec 1 samochod", 1, wynajmyZakonczone3.size());
         wypozyczalnia.zwrocSamochod("test1", "WYNAJEM-1");
+        // zwroc samochod zwraca exception bo samochod nie istnieje
+    }
+     // Znajdź samochód zwraca KAŻDY SAMOCHÓD (musisz potwierdzić, że niezależnie od tego czy samochód jest:
+            //      dostępny,
+            //      wynajęty czy
+            //      niedostępny
+            //  - możliwe jest znalezienie samochodu metodą znajdzSamochod
+@Test
+public void test_znajdzSamochod(){
+        Wypozyczalnia wypozyczalnia = new Wypozyczalnia();
+    Samochod autoTest1 = new Samochod("test1", SkrzyniaBiegow.AUTOMATYCZNA, TypNadwozia.CABRIO, StatusSamochodu.DOSTEPNY);
+    Samochod autoTest2 = new Samochod("test2", SkrzyniaBiegow.MANUAL, TypNadwozia.SUV, StatusSamochodu.WYNAJETY);
+    Samochod autoTest3 = new Samochod("test3", SkrzyniaBiegow.AUTOMATYCZNA, TypNadwozia.SEDAN, StatusSamochodu.NIEDOSTEPNY);
+    wypozyczalnia.dodajSamochod(
+            autoTest1.getNumerRejestracyjny(),
+            autoTest1.getSkrzynia(),
+            autoTest1.getTyp(),
+            autoTest1.getStatus());
+    wypozyczalnia.dodajSamochod(
+            autoTest2.getNumerRejestracyjny(),
+            autoTest2.getSkrzynia(),
+            autoTest2.getTyp(),
+            autoTest2.getStatus());
+    wypozyczalnia.dodajSamochod(
+            autoTest3.getNumerRejestracyjny(),
+            autoTest3.getSkrzynia(),
+            autoTest3.getTyp(),
+            autoTest3.getStatus());
+    List<Samochod> wszystkieSamochody = wypozyczalnia.zwrocListe();
+    Assert.assertEquals("Lista powinna zwierac 3 auta", 3, wszystkieSamochody.size());
+
+    // nie jest skonczony ten test wyzej
+
+}
+// przetestowac:
+ // Możliwe jest wynajęcie dwóch samochodów (tutaj mamy bug'a, trzeba poprawić kod)
+            // stwórz samochód sam1, sam2
+            // dodaj oba samochody
+            // wynajmij samochod sam1 (sprawdz dlugosci poszczegolnych list)
+            // wynajmij samochod sam2 (sprawdz dlugosci poszczegolnych list)
+
+    @Test
+    public void test_mozliweJestWynajecieDwochSamochodow(){
+        Wypozyczalnia wypozyczalnia = new Wypozyczalnia();
+        Samochod carTest1 = new Samochod("GD624CR", SkrzyniaBiegow.AUTOMATYCZNA, TypNadwozia.SEDAN, StatusSamochodu.DOSTEPNY);
+        Samochod carTest2 = new Samochod("NEL37349", SkrzyniaBiegow.AUTOMATYCZNA, TypNadwozia.SUV, StatusSamochodu.DOSTEPNY);
+        wypozyczalnia.dodajSamochod(
+                carTest1.getNumerRejestracyjny(),
+                carTest1.getSkrzynia(),
+                carTest1.getTyp(),
+                carTest1.getStatus());
+        wypozyczalnia.dodajSamochod(
+                carTest2.getNumerRejestracyjny(),
+                carTest2.getSkrzynia(),
+                carTest2.getTyp(),
+                carTest2.getStatus());
+        List<Samochod> wszystkieCary = wypozyczalnia.zwrocListe();
+        Assert.assertEquals("lista zawiera 2 cary", 2, wszystkieCary.size());
+        List<Samochod> wynajeteCary = wypozyczalnia.zwrocListeWynajetych();
+        Assert.assertEquals("Lista wynajetych nie ma zadnych car'ow", 0, wynajeteCary.size());
+        wypozyczalnia.wynajmij("NEL37349", "Kot Lorek", 5);
+        List<Samochod> wynajeteCary2 = wypozyczalnia.zwrocListeWynajetych();
+        List<Samochod> wszystkieCary2 = wypozyczalnia.zwrocListe();
+        List<WynajemSamochodu> wynajmyCarow2 = wypozyczalnia.listaAktywnychWynajmów();
+        Assert.assertEquals("Lista wynajetych car'ow powinna zwierac 1 car", 1, wynajeteCary2.size());
+        Assert.assertEquals("Lista wszystkich car'won powinna zawierac 2 car'y", 2, wszystkieCary2.size());
+        Assert.assertEquals("Lista wynajmow powinna zawierac 1 car", 1, wynajmyCarow2.size());
+        wypozyczalnia.wynajmij("GD624CR", "Kotka Bella", 3);
+        List<Samochod> wynajeteCary3 = wypozyczalnia.zwrocListeWynajetych();
+        List<Samochod> wszystkieCary3 = wypozyczalnia.zwrocListe();
+        List<WynajemSamochodu> wynajmyCarow3 = wypozyczalnia.listaAktywnychWynajmów();
+        Assert.assertEquals("Lista wynajetych car'ow powinna zwierac 1 car", 2, wynajeteCary3.size());
+        Assert.assertEquals("Lista wszystkich car'won powinna zawierac 2 car'y", 2, wszystkieCary3.size());
+        Assert.assertEquals("Lista wynajmow powinna zawierac 1 car", 2, wynajmyCarow3.size());
 
     }
 
-
-    // zwroc samochod zwraca exception bo samochod nie istnieje
 
     @Test
     public void test_uzytkownikNieZepsujeMetodyZmianyStatusuNaNiedostepnyPrzekazujacNieistniejacySamochod() {
